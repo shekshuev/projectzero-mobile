f4145e173685f2b63606687872b53298772113df
Sergei Shekshuev <sergei.shekshuev@gmail.com>
2023-01-06 00:06:05 +0300

    active survey changes

--- src/screens/ActiveSurvey.jsx
+++ src/screens/ActiveSurvey.jsx
@@ -15,7 +15,7 @@ const QuestionHeader = ({ question }) => {
 };
 
 const Single = ({ question, onAnswerSelected }) => {
-    const [value, setValue] = useState();
+    const [value, setValue] = useState(question.selectedAnswer);
 
     const onValueChanged = value => {
         setValue(value);
@@ -24,9 +24,9 @@ const Single = ({ question, onAnswerSelected }) => {
 
     return (
         <View>
-            <QuestionHeader question={question} />
+            <QuestionHeader question={question.question} />
             <RadioButton.Group onValueChange={newValue => onValueChanged(newValue)} value={value}>
-                {question.answers.map((answer, i) => (
+                {question?.question?.answers?.map((answer, i) => (
                     <RadioButton.Item label={answer.text} value={answer.code} key={i} />
                 ))}
             </RadioButton.Group>
@@ -35,7 +35,7 @@ const Single = ({ question, onAnswerSelected }) => {
 };
 
 const Multiple = ({ question, onAnswerSelected }) => {
-    const [values, setValues] = useState([]);
+    const [values, setValues] = useState(question.selectedAnswer || []);
 
     const onValueChanged = value => {
         let tmp = [];
@@ -47,10 +47,11 @@ const Multiple = ({ question, onAnswerSelected }) => {
         setValues(tmp);
         onAnswerSelected(tmp);
     };
+
     return (
         <View>
-            <QuestionHeader question={question} />
-            {question.answers.map((answer, i) => (
+            <QuestionHeader question={question.question} />
+            {question?.question?.answers?.map((answer, i) => (
                 <Checkbox.Item
                     label={answer.text}
                     onPress={() => onValueChanged(answer.code)}
@@ -63,7 +64,7 @@ const Multiple = ({ question, onAnswerSelected }) => {
 };
 
 const Open = ({ question, onAnswerSelected }) => {
-    const [value, setValue] = useState();
+    const [value, setValue] = useState(question.selectedAnswer);
 
     const onValueChanged = value => {
         setValue(value);
@@ -72,11 +73,11 @@ const Open = ({ question, onAnswerSelected }) => {
 
     return (
         <View>
-            <QuestionHeader question={question} />
+            <QuestionHeader question={question.question} />
             <TextInput
                 mode="flat"
                 value={value}
-                label={question?.answers?.[0]?.text}
+                label={question?.question?.answers?.[0]?.text}
                 onChangeText={onValueChanged}
                 style={{ marginBottom: 10 }}
             />
@@ -92,22 +93,57 @@ const ActiveSurvey = ({ route }) => {
     const survey = useMemo(() => surveys?.find(s => s.id === id), [surveys]);
 
     const [filled, setFilled] = useState({});
-    const [questions, setQuestions] = useState();
-    const [activeQuestion, setActiveQuestion] = useState();
-    const [activeIndex, setActiveIndex] = useState(0);
-    const [selectedAnswer, setSelectedAnswer] = useState();
+
+    const [activeIndex, setActiveIndex] = useState();
+
+    const questions = useMemo(() => filled.questions, [filled]);
+    const activeQuestion = useMemo(() => questions?.[activeIndex], [activeIndex]);
+    const canGoNext = useMemo(() => {
+        if (!questions?.[activeIndex]?.question?.required) {
+            return true;
+        } else if (!!questions?.[activeIndex]?.selectedAnswer) {
+            if (
+                Object.prototype.toString.apply(questions?.[activeIndex]?.selectedAnswer) === "[object Array]" &&
+                questions?.[activeIndex]?.selectedAnswer?.length === 0
+            ) {
+                return false;
+            }
+            return true;
+        }
+        return false;
+    }, [filled]);
 
     const onNextQuestionButtonClicked = () => {
-        if (activeIndex === questions?.length - 1) {
-            setActiveIndex(0);
-        } else {
-            setActiveIndex(activeIndex + 1);
-        }
+        setFilled({
+            ...filled,
+            questions: questions?.map((q, i) => {
+                if (i === activeIndex) {
+                    return {
+                        ...q,
+                        endDate: new Date()
+                    };
+                } else {
+                    return q;
+                }
+            })
+        });
+        setActiveIndex(activeIndex === questions?.length - 1 ? 0 : activeIndex + 1);
     };
 
     const onAnswerSelected = answer => {
-        console.log(answer);
-        setSelectedAnswer(answer);
+        setFilled({
+            ...filled,
+            questions: questions?.map((q, i) => {
+                if (i === activeIndex) {
+                    return {
+                        ...q,
+                        selectedAnswer: answer
+                    };
+                } else {
+                    return q;
+                }
+            })
+        });
     };
 
     useEffect(() => {
@@ -123,28 +159,51 @@ const ActiveSurvey = ({ route }) => {
                     coordinates: [currentPosition.longitude, currentPosition.latitude]
                 }
             },
-            questions: []
+            questions: survey.form.questions?.map(q => ({
+                question: q,
+                selectedAnswer: null,
+                beginDate: null,
+                endDate: null
+            }))
         });
-        setQuestions(survey.form.questions);
+        setActiveIndex(0);
     }, []);
 
     useEffect(() => {
-        setActiveQuestion(survey.form.questions?.[activeIndex]);
+        if (questions) {
+            setFilled({
+                ...filled,
+                questions: questions?.map((q, i) => {
+                    if (i === activeIndex) {
+                        return {
+                            ...q,
+                            beginDate: new Date()
+                        };
+                    } else {
+                        return q;
+                    }
+                })
+            });
+        }
     }, [activeIndex]);
 
     return (
         <View style={styles.container}>
-            {activeQuestion?.type === "single" ? (
+            {activeQuestion?.question?.type === "single" ? (
                 <Single question={activeQuestion} onAnswerSelected={onAnswerSelected} />
-            ) : activeQuestion?.type === "multiple" ? (
+            ) : activeQuestion?.question?.type === "multiple" ? (
                 <Multiple question={activeQuestion} onAnswerSelected={onAnswerSelected} />
-            ) : activeQuestion?.type === "open" ? (
+            ) : activeQuestion?.question?.type === "open" ? (
                 <Open question={activeQuestion} onAnswerSelected={onAnswerSelected} />
             ) : (
                 ""
             )}
             {questions?.length > 0 && (
-                <Button style={styles.button} mode="contained" onPress={onNextQuestionButtonClicked}>
+                <Button
+                    style={styles.button}
+                    disabled={!canGoNext}
+                    mode="contained"
+                    onPress={onNextQuestionButtonClicked}>
                     {t("screens.activeSurvey.next")}
                 </Button>
             )}
