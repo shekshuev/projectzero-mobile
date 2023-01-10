2112d3777d3cddbc6816bca371a40122994ad926
Sergei Shekshuev <sergei.shekshuev@gmail.com>
2023-01-10 23:39:50 +0300

    settings fix

--- src/screens/SettingsScreen.jsx
+++ src/screens/SettingsScreen.jsx
@@ -1,7 +1,7 @@
 import React, { useMemo, useState } from "react";
 import { StyleSheet, View, ScrollView } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
-import { Button, Portal, Dialog, Text, RadioButton, List, useTheme, TextInput } from "react-native-paper";
+import { Button, Dialog, Text, RadioButton, List, useTheme, TextInput } from "react-native-paper";
 import { useTranslation } from "react-i18next";
 import * as Application from "expo-application";
 import { clear as clearLocation } from "@features/location/locationSlice";
@@ -70,89 +70,84 @@ const SettingsScreen = ({ navigation, route }) => {
     );
 
     return (
-        <ScrollView>
-            <List.Section>
-                <List.Subheader>{t("screens.settings.common.header")}</List.Subheader>
-                <List.Item
-                    title={t("screens.settings.common.language")}
-                    description={languages[currentLanguage]}
-                    onPress={openLanguageDialog}
-                />
-                <List.Item
-                    title={t("screens.settings.common.apiAddress")}
-                    description={apiAddress || t("screens.settings.common.noApiAddress")}
-                    onPress={openApiAddressDialog}
-                />
-            </List.Section>
-            <List.Section>
-                <List.Subheader>{t("screens.settings.about.header")}</List.Subheader>
-                <List.Item title={t("screens.settings.about.name")} description={Application.applicationName} />
-                <List.Item
-                    title={t("screens.settings.about.version")}
-                    description={Application.nativeApplicationVersion}
-                />
-            </List.Section>
-            <View style={styles.container}>
-                <Button textColor={theme.colors.error} onPress={openClearDialog}>
-                    {t("screens.settings.clear")}
-                </Button>
-            </View>
-            <Portal>
-                <Dialog visible={isApiAddressModalVisible} onDismiss={closeApiAddressDialog}>
-                    <Dialog.Content>
-                        <Text variant="bodyLarge">{t("screens.settings.apiAddressModal.title")}</Text>
-                        <TextInput
-                            mode="flat"
-                            value={tmpApiAddress}
-                            label={t("screens.settings.apiAddressModal.label")}
-                            onChangeText={setTmpApiAddress}
-                            style={{ marginTop: 10 }}
-                        />
-                    </Dialog.Content>
-                    <Dialog.Actions>
-                        <Button onPress={onSetApiAddressButtonClicked}>
-                            {t("screens.settings.apiAddressModal.ok")}
-                        </Button>
-                        <Button onPress={closeApiAddressDialog}>{t("screens.settings.apiAddressModal.cancel")}</Button>
-                    </Dialog.Actions>
-                </Dialog>
-            </Portal>
-            <Portal>
-                <Dialog visible={isClearModalVisible} onDismiss={closeClearDialog}>
-                    <Dialog.Content>
-                        <Text variant="bodyLarge">{t("screens.settings.clearModal.title")}</Text>
-                    </Dialog.Content>
-                    <Dialog.Actions>
-                        <Button textColor={theme.colors.error} onPress={onClearButtonClicked}>
-                            {t("screens.settings.clearModal.yes")}
-                        </Button>
-                        <Button onPress={closeClearDialog}>{t("screens.settings.clearModal.cancel")}</Button>
-                    </Dialog.Actions>
-                </Dialog>
-            </Portal>
-            <Portal>
-                <Dialog visible={isLanguageModalVisible} onDismiss={closeLanguageDialog}>
-                    <Dialog.Content>
-                        <Text variant="bodyLarge">{t("screens.settings.languageModal.title")}</Text>
-                    </Dialog.Content>
-                    <Dialog.Content>
-                        <RadioButton.Group onValueChange={onLanguageSelected} value={currentLanguage}>
-                            <View style={styles.languageItem}>
-                                <RadioButton value="ru" />
-                                <Text>{t("screens.settings.languageModal.ru")}</Text>
-                            </View>
-                            <View style={styles.languageItem}>
-                                <RadioButton value="en" />
-                                <Text>{t("screens.settings.languageModal.en")}</Text>
-                            </View>
-                        </RadioButton.Group>
-                    </Dialog.Content>
-                    <Dialog.Actions>
-                        <Button onPress={closeLanguageDialog}>{t("screens.settings.languageModal.cancel")}</Button>
-                    </Dialog.Actions>
-                </Dialog>
-            </Portal>
-        </ScrollView>
+        <>
+            <ScrollView>
+                <List.Section>
+                    <List.Subheader>{t("screens.settings.common.header")}</List.Subheader>
+                    <List.Item
+                        title={t("screens.settings.common.language")}
+                        description={languages[currentLanguage]}
+                        onPress={openLanguageDialog}
+                    />
+                    <List.Item
+                        title={t("screens.settings.common.apiAddress")}
+                        description={apiAddress || t("screens.settings.common.noApiAddress")}
+                        onPress={openApiAddressDialog}
+                    />
+                </List.Section>
+                <List.Section>
+                    <List.Subheader>{t("screens.settings.about.header")}</List.Subheader>
+                    <List.Item title={t("screens.settings.about.name")} description={Application.applicationName} />
+                    <List.Item
+                        title={t("screens.settings.about.version")}
+                        description={Application.nativeApplicationVersion}
+                    />
+                </List.Section>
+                <View style={styles.container}>
+                    <Button textColor={theme.colors.error} onPress={openClearDialog}>
+                        {t("screens.settings.clear")}
+                    </Button>
+                </View>
+            </ScrollView>
+            <Dialog visible={isApiAddressModalVisible} onDismiss={closeApiAddressDialog}>
+                <Dialog.Content>
+                    <Text variant="bodyLarge">{t("screens.settings.apiAddressModal.title")}</Text>
+                    <TextInput
+                        mode="flat"
+                        value={tmpApiAddress}
+                        label={t("screens.settings.apiAddressModal.label")}
+                        onChangeText={setTmpApiAddress}
+                        autoCorrect={false}
+                        style={{ marginTop: 10 }}
+                    />
+                </Dialog.Content>
+                <Dialog.Actions>
+                    <Button onPress={onSetApiAddressButtonClicked}>{t("screens.settings.apiAddressModal.ok")}</Button>
+                    <Button onPress={closeApiAddressDialog}>{t("screens.settings.apiAddressModal.cancel")}</Button>
+                </Dialog.Actions>
+            </Dialog>
+            <Dialog visible={isClearModalVisible} onDismiss={closeClearDialog}>
+                <Dialog.Content>
+                    <Text variant="bodyLarge">{t("screens.settings.clearModal.title")}</Text>
+                </Dialog.Content>
+                <Dialog.Actions>
+                    <Button textColor={theme.colors.error} onPress={onClearButtonClicked}>
+                        {t("screens.settings.clearModal.yes")}
+                    </Button>
+                    <Button onPress={closeClearDialog}>{t("screens.settings.clearModal.cancel")}</Button>
+                </Dialog.Actions>
+            </Dialog>
+            <Dialog visible={isLanguageModalVisible} onDismiss={closeLanguageDialog}>
+                <Dialog.Content>
+                    <Text variant="bodyLarge">{t("screens.settings.languageModal.title")}</Text>
+                </Dialog.Content>
+                <Dialog.Content>
+                    <RadioButton.Group onValueChange={onLanguageSelected} value={currentLanguage}>
+                        <View style={styles.languageItem}>
+                            <RadioButton value="ru" />
+                            <Text>{t("screens.settings.languageModal.ru")}</Text>
+                        </View>
+                        <View style={styles.languageItem}>
+                            <RadioButton value="en" />
+                            <Text>{t("screens.settings.languageModal.en")}</Text>
+                        </View>
+                    </RadioButton.Group>
+                </Dialog.Content>
+                <Dialog.Actions>
+                    <Button onPress={closeLanguageDialog}>{t("screens.settings.languageModal.cancel")}</Button>
+                </Dialog.Actions>
+            </Dialog>
+        </>
     );
 };
 
